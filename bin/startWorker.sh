b2fa4af0864b83c18a242d4639f77da28178eac2
Miquel Adrover <miquel@adrover.info>
2013-06-18 01:54:56 +0200

    celery start scripts

---
+++ bin/startWorker.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ps -ef | grep manage.py | grep celery | awk '{print $2}' | xargs kill -9
+python manage.py celery flower &
+
+cd ..
+while getopts ":d" opt; do
+  case $opt in
+    d)
+      python manage.py celery worker --concurrency=10 -E -B --loglevel=debug
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+python manage.py celery worker --concurrency=10 -E -B --loglevel=info
