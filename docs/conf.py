1100804a8b29f189a500deb4306b099e8f32de98
Miquel Adrover <miquel@adrover.info>
2013-06-21 15:38:27 +0200

    docs updated

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'PyScaler'
+project = u'PyScaler. Monitoring and automating cloud deployments.'
 copyright = u'2013, Miquel Adrover'
 
 # The version info for the project you're documenting, acts as replacement for
